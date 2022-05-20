0ffb1f566399d2321e14c61b16f383c4111e56d2
Vineet Kumar <jammackp@gmail.com>
2022-05-20 12:44:15 +0530

    fix: unwanted consoles removed

--- scripts/header.js
+++ scripts/header.js
@@ -5,11 +5,9 @@ let docContent = document.querySelector(".doc-content");
 menu.addEventListener("click", () => {
   let menuIcon = document.getElementById("menu-icon");
   if (menuIcon.classList.contains("bi-list")) {
-    console.log("if case " + menuIcon.classList);
     menuIcon.classList.replace("bi-list", "bi-x");
     console.log("if case " + menuIcon.classList);
   } else {
-    console.log("else case " + menuIcon.classList);
     menuIcon.classList.replace("bi-x", "bi-list");
     console.log("else case " + menuIcon.classList);
   }
