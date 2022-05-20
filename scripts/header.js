f69a42aaf81f0926c24f604b4b5d1d882ee2d625
Vineet Kumar <jammackp@gmail.com>
2022-05-20 12:44:39 +0530

    fix: responsive fix

--- scripts/header.js
+++ scripts/header.js
@@ -1,15 +1,16 @@
 let menu = document.querySelector(".menu");
 let sidebar = document.querySelector(".sidebar");
+let docContent = document.querySelector(".doc-content");
+
 menu.addEventListener("click", () => {
   let menuIcon = document.getElementById("menu-icon");
   if (menuIcon.classList.contains("bi-list")) {
-    console.log("if case " + menuIcon.classList);
     menuIcon.classList.replace("bi-list", "bi-x");
     console.log("if case " + menuIcon.classList);
   } else {
-    console.log("else case " + menuIcon.classList);
     menuIcon.classList.replace("bi-x", "bi-list");
     console.log("else case " + menuIcon.classList);
   }
   sidebar.classList.toggle("sidebar-active");
+  docContent.classList.toggle("hide-doc");
 });
